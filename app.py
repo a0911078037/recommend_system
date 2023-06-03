2809c7832227dd3bd35ad7af8deceeb528fd8914
a0911078037 <a0911078037@yahoo.com.tw>
2023-06-03 13:37:38 +0800

    flask cors update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restx import Api
+from flask_cors import CORS
 from utility.EnvLoader import load_env_file
 import api
 
@@ -9,6 +10,7 @@ config = load_env_file('./env/api_config.yml')
 if __name__ == '__main__':
     app = Flask(__name__)
     app.register_blueprint(api.bp, url_prefix='/api')
+    CORS(app)
     Api = Api(app)
 
     if config['API']['VERSION'] != 'v1':
